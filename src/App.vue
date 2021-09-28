c1409c0aaeddd93dee40995b3d9ab880b94b2a6f
Esteban Damazio <edamazzio@Estebans-MacBook-Pro.local>
2021-09-28 07:12:51 -0600

    Youtube video browser app made in Vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <SearchBar @termChange="onTermChange"></SearchBar>
+    <div class="row">
+      <VideoDetail :video="selectedVideo"/>
+      <VideoList :videos="videos" @videoSelect="onVideoSelect"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import SearchBar from "@/components/SearchBar";
+import VideoList from "@/components/VideoList";
+import VideoDetail from "@/components/VideoDetail";
+
+const API_KEY = 'AIzaSyCX9gxl3HXO9ntIJ_F2DsV2RJlVfLDhmyA';
+const YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function onTermChange(searchTerm) {
+  axios.get(YOUTUBE_API_URL, {
+    params: {
+      key: API_KEY,
+      type: 'video',
+      part: 'snippet',
+      q: searchTerm
+    }
+  }).then(result => {
+    this.videos = result.data.items;
+  }).catch(err => {
+    console.error(err);
+  })
+}
+
+function onVideoSelect(video) {
+  this.selectedVideo = video;
+}
 
 export default {
   name: 'App',
+  data() {
+    return {
+      videos: [],
+      selectedVideo: null
+    }
+  },
   components: {
-    HelloWorld
+    VideoDetail,
+    VideoList,
+    SearchBar,
+  },
+  methods: {
+    onTermChange,
+    onVideoSelect
   }
 }
 </script>
@@ -21,8 +61,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/VideoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul class="list-group col-md-4">
+    <VideoListItem
+        v-for="video in videos"
+        :video="video"
+        :key="video.etag"
+        @videoSelect="onVideoSelect"
+    >
+
+    </VideoListItem>
+  </ul>
+</template>
+
+<script>
+import VideoListItem from "@/components/VideoListItem";
+export default {
+  name: "VideoList",
+  components: {VideoListItem},
+  methods: {
+    onVideoSelect(video){
+      this.$emit("videoSelect", video)
+    }
+  },
+  props: {
+    videos: Array,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
